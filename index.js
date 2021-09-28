3e7051f294ae0ccc537cb2b5dd817e18331857da
Nazar Bodan <nazar.bodan@indeema.com>
2021-09-28 12:37:47 +0300

    refactor case reversion script

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ function changeStrRegister(str="tEST"){
     let strResult = "";
     for (let i = 0; i < str.length; i++){
         if (str[i].toUpperCase() == str[i]){
-            strResult += str[i].toLowerCase()
-        } else { strResult += str[i].toUpperCase() }
+            strResult += str[i].toLowerCase();
+        } else {
+            strResult += str[i].toUpperCase();
+        }
     }
     return strResult;
 }
 
 
-console.log(changeStrRegister('ASdsASDS'))
+console.log(changeStrRegister('ASdsASDS'));
